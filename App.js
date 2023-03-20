5d3c6e995a0dc7d1aa9c14e085642fd5198f8218
IchiIchigo <lehoannamkha2002@gmail.com>
2023-03-20 17:29:21 +0700

    Move styling to seperated files

--- App.js
+++ App.js
@@ -1,67 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import {
-  Provider as PaperProvider,
-  MD3DarkTheme as DefaultTheme,
+    Provider as PaperProvider,
+    MD3DarkTheme as DefaultTheme,
 } from "react-native-paper";
+import theme from "./theme";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as ReduxProvider } from "react-redux";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
-import { NavigationContainer } from "@react-navigation/native";
+// import HomeView from "./views/HomeView";
+import * as native from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-const theme = {
-  ...DefaultTheme,
-  myOwnProperty: true,
-  colors: {
-    primary: "#A1C9FF",
-    primaryContainer: "#00487F",
-    onPrimary: "#00325A",
-    onPrimaryContainer: "#D2E4FF",
-    secondary: "#BBC7DB",
-    secondaryContainer: "#3C4858",
-    onSecondary: "#253141",
-    onSecondaryContainer: "#D7E3F8",
-    tertiary: "#D8BDE4",
-    tertiaryContainer: "#533F5F",
-    onTertiary: "#3C2947",
-    onTertiaryContainer: "#F4D9FF",
-    error: "#FFB4AB",
-    errorContainer: "#93000A",
-    onError: "#690005",
-    onErrorContainer: "#FFDAD6",
-    outline: "#8D9199",
-    background: "#1A1C1E",
-    onBackground: "#E3E2E6",
-    surface: "#1A1C1E",
-    onSurface: "#C6C6CA",
-    surfaceVariant: "#43474E",
-    onSurfaceVariant: "#C3C6CF",
-    inverseSurface: "#E3E2E6",
-    inverseOnSurface: "#1A1C1E",
-    inversePrimary: "#0F61A4",
-    shadow: "#000000",
-    surfaceTint: "#A1C9FF",
-    outlineVariant: "#43474E",
-    scrim: "#000000",
-    neuralVariant: "#4E535A",
-  },
-};
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    // <ReduxProvider>
-    <PaperProvider theme={theme}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false, animation: "slide_from_right"}}>
-        <Stack.Screen name="Login" component={LoginView} />
-        <Stack.Screen name="Register" component={RegisterView} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    </PaperProvider>
-    // </ReduxProvider>
-  );
+    return (
+        // <ReduxProvider>
+        <PaperProvider theme={theme}>
+            <native.NavigationContainer theme={theme}>
+                <Stack.Navigator
+                    initialRouteName='Login'
+                    screenOptions={{
+                        headerShown: false,
+                        animation: "slide_from_right",
+                    }}
+                >
+                    <Stack.Screen name='Login' component={LoginView} />
+                    <Stack.Screen name='Register' component={RegisterView} />
+                    {/* <Stack.Screen name="Home" component={HomeView} /> */}
+                </Stack.Navigator>
+            </native.NavigationContainer>
+        </PaperProvider>
+        // </ReduxProvider>
+    );
 }
 
 // const styles = StyleSheet.create({
--- views/LoginView.jsx
+++ views/LoginView.jsx
@@ -1,54 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { withTheme, TextInput, Button } from "react-native-paper";
+import style from "./Style"
 
-function LoginView({ theme, navigation }) {
-    const style = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: theme.colors.background,
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        input: {
-            borderRadius: 5,
-            width: 300,
-            backgroundColor: theme.colors.neuralVariant,
-            color: theme.colors.onSurfaceVariant,
-            marginTop: 10,
-        },
-        button: {
-            marginTop: 10,
-        },
-        headerText: {
-            color: "#fff",
-            fontWeight: 700,
-            fontSize: 26,
-            marginTop: 30,
-        },
-        logoText: {
-            color: theme.colors.primary,
-            fontWeight: 900,
-            fontSize: 40,
-            marginBottom: 40,
-        },
-        forgotText: {
-            color: theme.colors.secondary,
-            marginVertical: 10,
-        },
-        guessText: {
-            textDecorationLine: "underline",
-            color: theme.colors.secondary,
-            textAlign: "center",
-            marginTop: 20,
-        },
-    });
+function LoginView({ navigation }) {
     return (
-        <View style={style.container}>
-            <Text style={style.logoText}>KomicBook</Text>
+        <View style={style(style).container}>
+            <Text style={style(style).logoText}>KomicBook</Text>
             <View>
-                <Text style={style.headerText}>Login</Text>
+                <Text style={style(style).headerText}>Login</Text>
                 <TextInput
-                    style={style.input}
+                    style={style(style).input}
                     mode="flat"
                     underlineColor="transparent"
                     activeUnderlineColor="transparent"
@@ -56,23 +17,25 @@ function LoginView({ theme, navigation }) {
                     left={<TextInput.Icon icon="account" />}
                 ></TextInput>
                 <TextInput
-                    style={style.input}
+                    style={style(style).input}
                     mode="flat"
                     underlineColor="transparent"
                     activeUnderlineColor="transparent"
                     placeholder="Password"
                     left={<TextInput.Icon icon="lock" />}
                 ></TextInput>
-                <Text style={style.forgotText}>Forgot your password?</Text>
+                <Text style={style(style).forgotText}>Forgot your password?</Text>
                 <Button mode="contained">Login</Button>
                 <Button
                     mode="outlined"
-                    style={style.button}
+                    style={style(style).button}
                     onPress={() => navigation.navigate("Register")}
                 >
                     Register
                 </Button>
-                <Text style={style.guessText}>Join As Guest</Text>
+                <Pressable>
+                    <Text style={style(style).guestText} onPress={() => navigation.navigate("Home")}>Join As Guest</Text>
+                </Pressable>
             </View>
         </View>
     );
