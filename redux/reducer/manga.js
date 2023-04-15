418446ba9dbe59d78cff65d5f2a36ec054ead41f
chative-phuc <phuc.nguyen@chative.io>
2023-04-16 00:21:06 +0700

    get latest chapter

--- redux/reducer/manga.js
+++ redux/reducer/manga.js
@@ -3,11 +3,14 @@ import axios from "axios";
 
 import { storeData, getData } from "../../features/asyncStorage";
 
-const url = 'http://192.168.1.8:3033/'
+const url = 'http://localhost:3032/'
 
 const initialState = {
     tag : null,
     manga : null,
+    listManga: null,
+    detailManga: null,
+    listChapter: null,
   }
 export const mangaSlice = createSlice({
     name : 'manga',
@@ -17,10 +20,31 @@ export const mangaSlice = createSlice({
         setTags : (state,action)=>{
             state.tag = action.payload
         },
+        setOneManga: (state,action) => {
+            state.manga = action.payload
+            console.log(state.manga)
+        },
+
+     
     }
 })
 
-export const {setTags} = mangaSlice.actions
+export const {setTags,setOneManga} = mangaSlice.actions
 
+export const randomManga = (payload) => (dispatch) => {
+    axios({
+        method: 'GET',
+        url:`${url}api/v1/manga/random`,
+    }).then((res) => {
+        dispatch(setOneManga(res.data.data));
+       
+    })
+}
 
+export const listChapter = (payload) => (dispatch) => {
+    axios({
+        method: 'GET',
+        url: `${url}`
+    })
+}
 export default mangaSlice.reducer
